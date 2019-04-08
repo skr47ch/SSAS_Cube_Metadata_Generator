4359eb6fe9396cb3b21305cd54eb44e6f4f8dac9
Shashank <its.me.arrived@gmail.com>
2019-04-08 19:25:10 +1000

    Lists all partitions in all measure groups in cubes

---
+++ ListPartitions.ps1
@@ -0,0 +1,38 @@
+param($ServerName=@("server1", "server2"))
+$output = ''
+
+# Headers
+$output += "Server Name,Database Name,Cube Name,Measure Group Name,Partition Name" 
+Write-Output($output)
+
+$output | Set-Content partitions.csv
+
+
+
+foreach($srvr in $ServerName) {
+    ## Add the AMO namespace
+    $loadInfo = [Reflection.Assembly]::LoadWithPartialName("Microsoft.AnalysisServices")
+    $server = New-Object Microsoft.AnalysisServices.Server
+
+    $server.connect($srvr)
+    if ($server.name -eq $null) {
+     Write-Output ("Server '{0}' not found" -f $srvr)
+     break
+    }
+
+    foreach ($database in $server.Databases) {
+        foreach($cube in $database.Cubes) {
+            foreach($measureGroup in $cube.MeasureGroups) {
+                foreach($partition in $measureGroup.Partitions) {
+                    ("{0},{1},{2},{3},{4}" -f 
+                    $server.Name,
+                    $database.Name,
+                    $cube.Name,
+                    $measureGroup.Name,
+                    $partition.Name) | Add-Content partitions.csv
+                }    
+            }     
+        }
+    }
+
+}
